4dce3d328922ba5768e58805468849627f9900f1
farinah26 <hussainrameez37@gmail.com>
2025-09-12 18:17:13 +0000

    Added TriggerComprehend Message

---
+++ project1-feedback-analysis/java/TriggerComprehend.java
@@ -0,0 +1,5 @@
+public class TriggerComprehend {
+    public static void main(String[] args) {
+        System.out.println("This script would trigger AWS Comprehend sentiment analysis for new feedback.");
+    }
+}
