dca7f5c71089ffefcfbe01d7477334b4a2c66e7d
Farina Hussain <hussainrameez37@gmail.com>
2026-08-24 13:47:11 -0700

    Align Java source layout with Maven conventions

--- project1-feedback-analysis/java/SNSAlert.java
+++
@@ -1,5 +0,0 @@
-public class SNSAlert {
-    public static void main(String[] args) {
-        System.out.println("This script would send an SNS alert if negative feedback is detected.");
-    }
-}
--- project1-feedback-analysis/java/TriggerComprehend.java
+++
@@ -1,5 +0,0 @@
-public class TriggerComprehend {
-    public static void main(String[] args) {
-        System.out.println("This script would trigger AWS Comprehend sentiment analysis for new feedback.");
-    }
-}
